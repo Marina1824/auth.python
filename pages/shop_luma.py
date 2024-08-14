7d1a0c1767d7533e7d4a44bdd2edbb1c4cf10a43
Marina1824 <9mariam7@gmail.com>
2024-08-14 16:14:40 +0300

    test_2

--- pages/shop_luma.py
+++ pages/shop_luma.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
 
@@ -16,24 +17,39 @@ class ShopLuma:
         new.click()
         yes = self.driver.find_element(By.XPATH, '//*[@id="narrow-by-list"]/div[9]/div[2]/ol/li[1]/a')
         yes.click()
-        sleep(5)
+        check_count = len(self.driver.find_elements(By.CLASS_NAME, 'product-image-container'))
+        total = (self.driver.find_element(By.ID, 'toolbar-amount')).text
+        number = int(total.split()[0])
+        assert check_count == number
 
-    def check_success_text(self, expected_text):
-        registering = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="maincontent"]/div[1]/div[2]/div/div/div'))
 
+    def compare_luma(self):
+        item = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'product-image-wrapper'))
         )
-        assert registering.text == expected_text
-
-    def check_text(self, expected_text):
-        registering = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="maincontent"]/div[2]'))
-
+        actions = ActionChains(self.driver)
+        actions.move_to_element(item).perform()
+        compare = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'tocompare'))
         )
-        assert registering.text == expected_text
-
-    def check_email(self, expected_text):
-        email = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="email_address-error"]'))
+        actions.click(compare).perform()
+        item_text = self.driver.find_element(By.CLASS_NAME, 'product-item-link').text
+        self.driver.execute_script("window.scrollTo(0, 500)")
+        compare_text = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="compare-items"]/li/strong/a'))
         )
-        assert email.text == expected_text
+        assert item_text == compare_text.text
+
+
+    def wishlist_luma(self, expected_text):
+        item = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'product-image-wrapper'))
+        )
+        actions = ActionChains(self.driver)
+        actions.move_to_element(item).perform()
+        wishlist = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'towishlist'))
+        )
+        actions.click(wishlist).perform()
+        item_text = self.driver.find_element(By.CLASS_NAME, 'messages').text
+        assert item_text == expected_text
--- test/test_2.py
+++ test/test_2.py
@@ -10,7 +10,17 @@ def driver():
     yield chrome_driver
 
 
-def test_auth(driver):
+def test_filter(driver):
     filter_item = ShopLuma(driver)
     filter_item.open_page()
     filter_item.filter()
+
+def test_compare(driver):
+    compare = ShopLuma(driver)
+    compare.open_page()
+    compare.compare_luma()
+
+def test_wishlist(driver):
+    compare = ShopLuma(driver)
+    compare.open_page()
+    compare.wishlist_luma('You must login or register to add items to your wishlist.')
