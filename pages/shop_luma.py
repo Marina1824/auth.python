6d7590103807d13177d8b5773bb56612879ff402
Marina1824 <9mariam7@gmail.com>
2024-08-14 17:33:42 +0300

    удаление sleep

--- pages/shop_luma.py
+++ pages/shop_luma.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
 
 
 class ShopLuma:
