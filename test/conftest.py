b381449b343bea4f274bd6c92be27dbd8e77688e
Marina1824 <9mariam7@gmail.com>
2024-12-04 19:39:42 +0200

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,15 +1,20 @@
 import pytest
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from pages.shop_luma import ShopLuma
 from pages.sales import Sales
 from pages.customer_login import Customerlogin
+from time import sleep
 
 
 @pytest.fixture()
 def driver():
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.maximize_window()
-    yield chrome_driver
+    options = Options()
+    options.add_argument('--headless')
+    chrome_driver = webdriver.Chrome(options=options)
+    sleep(3)
+    chrome_driver.implicitly_wait(5)
+    return chrome_driver
 
 
 @pytest.fixture()
